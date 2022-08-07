54d8d9f2f25f3e5257df560c9d722f646a9c5520
Ashutosh <ashutoshgupta2908@gmail.com>
2022-08-07 17:32:30 +0530

    uri updated

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config.update(
 )
 mail = Mail(app)
 app.secret_key = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://dctbaweftgwoor:66ca3e590119e8a8562f420de326678f54da27d94b8798d55311ba8c28586bde@ec2-18-214-35-70.compute-1.amazonaws.com:5432/deh27mlcom5tav"
 
 db = SQLAlchemy(app)
 s = Serializer(app.secret_key)
